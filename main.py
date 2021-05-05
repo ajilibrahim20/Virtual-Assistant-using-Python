246e7247a24bf393192dff40e356bc558296bde4
Ajil Ibrahim <ajilibrahim20@gmail.com>
2021-05-05 10:58:19 +0530

    initial commit with python

---
+++ main.py
@@ -0,0 +1,66 @@
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
+import pyjokes
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print('listening...')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if 'lakshmikutty' in command:
+                command = command.replace('lakshmikutty', '')
+                print(command)
+    except:
+        pass
+    return command
+
+
+def run_lakshmikutty():
+    command = take_command()
+    print(command)
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('playing ' + song)
+        pywhatkit.playonyt(song)
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%I:%M %p')
+        talk('Current time is ' + time)
+    elif 'who the heck is' in command:
+        person = command.replace('who the heck is', '')
+        info = wikipedia.summary(person, 1)
+        print(info)
+        talk(info)
+    elif 'date' in command:
+        talk('sorry, I have a headache')
+    elif 'are you single' in command:
+        talk('as being the smartest')
+    elif 'who is Ajil' in command:
+        talk('most powerful one')
+    elif 'thank you' in command:
+        talk('Welcome ')
+    elif 'joke' in command:
+        talk(pyjokes.get_joke())
+    elif 'bye' in command:
+        talk('see you')
+    else:
+        talk('Please say the command again.')
+
+
+while True:
+    run_lakshmikutty()
